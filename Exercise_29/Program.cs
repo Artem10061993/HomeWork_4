59e1aa5bac419a346ca6abb310e2ef66eea459d6
Artem Kandakov <Temofey1993@gmail.com>
2022-08-22 22:35:17 +0500

    Задача №29

---
+++ Exercise_29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] list = {1, 2, 3, 4, 5, 6, 7, 8};
+int i = 0;
+while(i<list.Length)
+{
+    if (i==0)
+    {Console.Write("[");}
+    if (i==7) 
+    {
+        Console.Write(list[i]+"]");
+    }
+    else
+    {
+    Console.Write(list[i]+", ");
+    }
+    i++;
+}
